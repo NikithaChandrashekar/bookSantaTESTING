8b391d6e588e1e553c9e0d99f17eac21aca1d903
NikithaChandrashekar <72256727+NikithaChandrashekar@users.noreply.github.com>
2021-06-30 17:03:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyApLn4n1H2qYW-ocWp9LX5-5vKIdFuPDa8",
+  authDomain: "booksanta-e3811.firebaseapp.com",
+  projectId: "booksanta-e3811",
+  storageBucket: "booksanta-e3811.appspot.com",
+  messagingSenderId: "377261479243",
+  appId: "1:377261479243:web:0bc72171380af2f23744ce",
+  measurementId: "G-G3K0E7K8G9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
